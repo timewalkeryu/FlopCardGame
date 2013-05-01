4f73d722d13fc9739170ba3490d91f787e229d5b
HyeonGyu Yu <timewalker.yu@uxlayer.com>
2013-05-01 17:54:08 +0900

    Initial Commit

---
+++ FlopCardGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  FlopCardGameAppController.h
+//  FlopCardGame
+//
+//  Created by HyeonGyu Yu on 13. 5. 1..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ FlopCardGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  FlopCardGameAppController.h
+//  FlopCardGame
+//
+//  Created by HyeonGyu Yu on 13. 5. 1..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
